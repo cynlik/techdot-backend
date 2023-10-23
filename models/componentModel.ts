1d8826937fd7614fe1912befc3625b135648b40f
Leandro Magalhães <57193254+cynlik@users.noreply.github.com>
2023-10-23 19:40:02 +0100

    Ya ignorem 2

--- models/componentModel.ts
+++ models/componentModel.ts
@@ -2,48 +2,34 @@ import { Schema } from 'mongoose';
 
 interface Component {
   name: string;
-  description: string;
-  componentType: ComponentType;
+  description?: string;
+  componentType: 'CPU' | 'PSU' | 'GPU' | 'MOTHERBOARD' | 'RAM' | 'SSD' | 'HDD';
   manufacturer: string; //MARCA QUE FAZ O COMPONENTE
   price: number;
   specifications: {
     // estas abaixo sao opcionais
-    date: Date;
-    socketType: string;
-    graphicsType: string;
-    capacity: string;
-    watts: string;
-    speed: string; //in MhZ Ghz IOPS MB/s
+    date?: Date;
+    socketType?: string;
+    graphicsType?: string;
+    capacity?: string;
+    watts?: string;
+    speed?: string; //in MhZ Ghz IOPS MB/s
   };
 }
-
-interface ComponentType {
-  name: 'CPU' | 'PSU' | 'GPU' | 'MOTHERBOARD' | 'RAM' | 'SSD' | 'HDD';
-}
-
-// Define a schema for component types
-// CPU, PSU, GPU, MB e RAM
-const componentType = new Schema<ComponentType>({
-  name: {
-    type: String,
-    enum: ['CPU', 'PSU', 'GPU', 'MOTHERBOARD', 'RAM', 'SSD', 'HDD'],
-  },
-});
 
 // Define a schema for PC components
 const componentSchema = new Schema<Component>({
   name: { type: String, required: true },
   description: String,
   componentType: {
-    //usar isto para filtrar entre os diferentes componentes nas queries no menu
-    type: Schema.Types.ObjectId,
-    ref: 'ComponentType',
+    type: String,
+    enum: ['CPU', 'PSU', 'GPU', 'MOTHERBOARD', 'RAM', 'SSD', 'HDD'],
   },
   manufacturer: { type: String, required: true }, //MARCA QUE FAZ O COMPONENTE
   price: { type: Number, required: true },
   specifications: {
-		date: Date,
     // estas abaixo sao opcionais
+    date: Date,
     socketType: String,
     graphicsType: String,
     capacity: String,
@@ -53,10 +39,8 @@ const componentSchema = new Schema<Component>({
   //image: String, //a ser adicionado futuramente
 });
 
-const ComponentType = mongoose.model('ComponentType', componentType);
 const Component = mongoose.model('Component', componentSchema);
 
 module.exports = {
-  ComponentType,
   Component,
 };
