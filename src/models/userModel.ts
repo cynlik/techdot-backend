55810855a630128ef2a2665d7f4dfa850dc6531a
samusafe <samucarvalhocp@gmail.com>
2023-10-26 16:16:50 +0100

    TBF userController + userModel + componentModel

--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -1,23 +1,30 @@
-import { model, Document, Schema } from 'mongoose';
+import { Schema, Document, model } from 'mongoose';
 
 export interface IUser extends Document {
   name: string;
   email: string;
   password: string;
-  role: ('manage-products' | 'manage-clients' | 'member' | 'nonmember')[]; 
+  role: 'manage-products' | 'manage-clients' | 'member' | 'nonmember';
   age: number;
   address: string;
   country: string;
-};
+}
 
-export const UserSchema = new Schema<IUser>({
-  name: { type: String, required: true, unique: true },
-  email: { type: String, required: true, unique: true },
+const userSchema = new Schema<IUser>({
+  name: { type: String, required: true },
+  email: { type: String, required: true },
   password: { type: String, required: true },
-  role: { type: [String], required: true, default: ['member'] },
+  role: {
+    type: String,
+    required: true,
+    default: 'member',
+    enum: ['manage-products', 'manage-clients', 'member', 'nonmember'],
+  },
   age: { type: Number, required: true },
   address: { type: String, required: true },
   country: { type: String, required: true },
 });
 
-export const User = model<IUser>('User', UserSchema);
+const UserModel = model<IUser>('User', userSchema);
+
+export default UserModel;
