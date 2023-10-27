d09f5a42fc7b05d3da62106a016326ca0e18a36d
samusafe <samucarvalhocp@gmail.com>
2023-10-27 17:00:57 +0100

    userRoles

--- src/middlewares/userMiddleWare.ts
+++ src/middlewares/userMiddleWare.ts
@@ -1,19 +1,21 @@
 import { Request, Response, NextFunction } from 'express';
-import { IUser } from '@src/models/userModel';
+import { IUser } from '@src/models/userModel'; 
+import { UserRole } from '@src/utility/roles'; 
 
 interface CustomRequest extends Request {
   user: IUser;
 }
 
-const roleMiddleware = (allowedRole: string) => {
+const roleMiddleware = (allowedRole: UserRole) => {
   return async (req: CustomRequest, res: Response, next: NextFunction) => {
     try {
       if (!req.user) {
         return res.status(401).json({ message: 'Unauthorized access' });
       }
 
-      const role = req.user.role;
-      if (role !== allowedRole) {
+      const roles = req.user.roles;
+
+      if (!roles.includes(allowedRole)) {
         return res
           .status(403)
           .json({ message: `Access denied. Only ${allowedRole} users allowed.` });
@@ -28,10 +30,10 @@ const roleMiddleware = (allowedRole: string) => {
 
 export { roleMiddleware };
 
-/** 
- * Exemplo utilizaçao
+/**
+ * Create a new role
  * 
- * @param {string}
- * const productManagerMiddleware = roleMiddleware('manage-products');
- * const clientManagerMiddleware = roleMiddleware('manage-clients');
-*/
+ * @param {UserRole.role}
+ * const productManagerMiddleware = roleMiddleware(UserRole.ManageProducts);
+ * const clientManagerMiddleware = roleMiddleware(UserRole.ManageClients);
+ */
--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -1,30 +1,29 @@
-import { Schema, Document, model } from 'mongoose';
+import { model, Document, Schema } from 'mongoose';
+import { UserRole } from '@src/utility/roles';
 
 export interface IUser extends Document {
   name: string;
   email: string;
   password: string;
-  role: 'manage-products' | 'manage-clients' | 'member' | 'nonmember';
+  roles: UserRole[];
   age: number;
   address: string;
   country: string;
 }
 
-const userSchema = new Schema<IUser>({
-  name: { type: String, required: true },
-  email: { type: String, required: true },
+export const UserSchema = new Schema<IUser>({
+  name: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  role: {
-    type: String,
+  roles: {
+    type: [String],
     required: true,
-    default: 'member',
-    enum: ['manage-products', 'manage-clients', 'member', 'nonmember'],
+    default: [UserRole.Member],
+    enum: Object.values(UserRole),
   },
   age: { type: Number, required: true },
   address: { type: String, required: true },
   country: { type: String, required: true },
 });
 
-const UserModel = model<IUser>('User', userSchema);
-
-export default UserModel;
+export const User = model<IUser>('User', UserSchema);
