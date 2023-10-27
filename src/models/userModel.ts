a99d818d7dc051923ad25cf7cc1f38e2112554e1
samusafe <samucarvalhocp@gmail.com>
2023-10-27 17:00:57 +0100

    userRoles

--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -1,24 +1,25 @@
 import { model, Document, Schema } from 'mongoose';
+import { UserRole } from '@src/utility/roles';
 
 export interface IUser extends Document {
   name: string;
   email: string;
   password: string;
-  role: 'manage-products' | 'manage-clients' | 'member' | 'nonmember';
+  roles: UserRole[];
   age: number;
   address: string;
   country: string;
-};
+}
 
 export const UserSchema = new Schema<IUser>({
   name: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  role: {
-    type: String,
+  roles: {
+    type: [String],
     required: true,
-    default: 'member',
-    enum: ['manage-products', 'manage-clients', 'member', 'nonmember'],
+    default: [UserRole.Member],
+    enum: Object.values(UserRole),
   },
   age: { type: Number, required: true },
   address: { type: String, required: true },
