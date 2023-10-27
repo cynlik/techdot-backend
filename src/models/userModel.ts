508420b981ebc5ff820f680d79e208b16bef79f3
samusafe <samucarvalhocp@gmail.com>
2023-10-27 16:23:43 +0100

    User role changed

--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -4,7 +4,7 @@ export interface IUser extends Document {
   name: string;
   email: string;
   password: string;
-  role: ('manage-products' | 'manage-clients' | 'member' | 'nonmember')[]; 
+  role: 'manage-products' | 'manage-clients' | 'member' | 'nonmember';
   age: number;
   address: string;
   country: string;
@@ -14,7 +14,12 @@ export const UserSchema = new Schema<IUser>({
   name: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  role: { type: [String], required: true, default: ['member'] },
+  role: {
+    type: String,
+    required: true,
+    default: 'member',
+    enum: ['manage-products', 'manage-clients', 'member', 'nonmember'],
+  },
   age: { type: Number, required: true },
   address: { type: String, required: true },
   country: { type: String, required: true },
