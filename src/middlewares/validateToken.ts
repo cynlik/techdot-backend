67181bd3b037dcc12a538d674ee4de72a03b20ef
samusafe <samucarvalhocp@gmail.com>
2023-10-28 02:11:41 +0100

    validateToken

--- src/middlewares/validateToken.ts
+++ src/middlewares/validateToken.ts
@@ -1,31 +1,29 @@
 import { IUser } from '@src/models/userModel';
 import { Request, Response, NextFunction } from 'express';
-import asyncHandler from 'express-async-handler';
 import jwt from 'jsonwebtoken';
 
 interface CustomRequest extends Request {
   user?: IUser;
 }
 
-const validateToken = asyncHandler(async (req: CustomRequest, res: Response, next: NextFunction) => {
-  let token;
-  let authHeader = req.headers.authorization || req.headers.Authorization;
-  if (authHeader && typeof authHeader === 'string' && authHeader.toLowerCase().startsWith('bearer')) {
-    token = authHeader.split(' ')[1];
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET as string, (err, decoded) => {
-      if (err) {
-        res.status(401);
-        throw new Error('User is not authorized');
-      }
+const validateToken = async (req: CustomRequest, res: Response, next: NextFunction) => {
+  try {
+    let token;
+    let authHeader = req.headers.authorization || req.headers.Authorization;
+    if (authHeader && typeof authHeader === 'string' && authHeader.toLowerCase().startsWith('bearer')) {
+      token = authHeader.split(' ')[1];
+      const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET as string);
       req.user = (decoded as { user: IUser }).user;
       next();
-    });
-
-    if (!token) {
+    } else {
       res.status(401);
       throw new Error('User is not authorized or token is missing');
     }
+  } catch (err: any) { // TODO: handle error message
+    res.status(401).json({ error: err.message });
   }
-});
+};
+
+
 
 export default validateToken;
