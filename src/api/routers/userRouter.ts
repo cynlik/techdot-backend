776d4333617a13a31eae58347a81a6be83a36aea
samusafe <samucarvalhocp@gmail.com>
2023-10-28 05:08:36 +0100

    verifyAccount

--- src/api/routers/userRouter.ts
+++ src/api/routers/userRouter.ts
@@ -5,6 +5,6 @@ const router = express.Router();
 const userController = new UserController();
 
 router.post('/register', (req, res) => userController.registerUser(req, res));
-router.post('/verify', (req, res) => userController.registerUser(req, res));
+router.post('/verify', (req, res) => userController.verifyAccount(req, res));
 
 export default router;
