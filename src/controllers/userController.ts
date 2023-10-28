0683629dbae783f9a231ad27fc426635e6526e34
Leandro Magalhães <leandrofrmaggalaes27@gmail.com>
2023-10-28 14:40:21 +0100

    get/update/remove user in usercontroller & router

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -8,7 +8,7 @@ import { EmailType } from '@src/utils/emailType';
 
 export default class UserController {
 
-  public async registerUser(req: Request, res: Response) {
+  static async registerUser(req: Request, res: Response) {
     try {
       const { name, email, password } = req.body;
       if (!name || !email || !password) {
@@ -51,29 +51,118 @@ export default class UserController {
     }
   }
 
-  public async verifyAccount(req: Request, res: Response) {
+  static async verifyAccount(req: Request, res: Response) {
     //
   }
 
-  private hashPassword(password: string) {
-    return bcrypt.hash(password, config.saltRounds).catch((err) => {
-      return Promise.reject(new Error(`Password not hashed, error: \n ${err}`));
-    });
+  static async findById(id: string): Promise<IUser> {
+    try {
+      const user = await User.findById(id).exec();
+      
+      if(user === null) {
+        throw new Error("User not found!");
+      }
+
+      return user;
+    } catch (err) {
+      return Promise.reject(err);
+    }
+  }
+
+  static async updateById(id: string, newUser: Partial<IUser>): Promise<IUser> {
+    try {
+      const dbUser = await User.findById(id).exec();
+
+      if(dbUser === null) {
+        throw new Error("User not found!");
+      }
+
+      if(newUser.email && !(await this.isEmailUnique(newUser.email))) {
+        throw new Error("Email already in use.");
+      }
+
+      if(newUser.password && !(await this.comparePassword(newUser.password, dbUser.password))) {
+        newUser.password = await this.hashPassword(newUser.password);
+      }else {
+        newUser.password = dbUser.password;
+      }
+
+      return await User.findByIdAndUpdate(id, newUser).exec() as IUser;
+    }catch (err) {
+      return Promise.reject(err);
+    }
+  }
+
+  static async removeById(id: string): Promise<IUser> {
+    try {
+      const user = await User.findByIdAndRemove(id).exec();
+
+      if(user === null) {
+        throw new Error("User not found!");
+      }
+
+      return user;
+    }catch (err) {
+      return Promise.reject(err);
+    }
+  }
+
+  // static async getUser(req: Request, res: Response) {
+  //   try {
+  //     const user = User.findById(req.body._id);
+  //     if (user) {
+  //       res.status(200).send(user);
+  //     }
+  //   } catch (error) {
+  //     res.status(404).send('User not found');
+  //   }
+  // }
+
+  // static async updateUser(req: Request, res: Response) {
+  //   try {
+  //     const user = User.findByIdAndRemove(req.body._id);
+  //     if (user) {
+  //       res.status(200).send(user);
+  //     }
+  //   } catch (error) {
+  //     res.status(404).send('User not found');
+  //   }
+  // }
+
+  // static async deleteUser(req: Request, res: Response) {
+  //   try {
+  //     const user = User.findByIdAndUpdate(req.body._id);
+  //     if (user) {
+  //       res.status(200).send(user);
+  //     }
+  //   } catch (error) {
+  //     res.status(404).send('User not found');
+  //   }
+  // }
+
+  private static async hashPassword(password: string): Promise<string> {
+    try {
+      return await bcrypt.hash(password, config.saltRounds);
+    }catch (err: any) {
+      err = err instanceof Error ? err : new Error(err);
+
+      return Promise.reject(new Error(`Password not hashed, error: \n ${err.message}`));
+    }
   }
   
-  private comparePassword(newPassword: string, hash: string) {
+  private static comparePassword(newPassword: string, hash: string) {
     return bcrypt.compare(newPassword, hash);
   }
 
-  private async isEmailUnique(newUser: IUser) {
-    return (await User.find({ email: newUser.email }).exec()).length <= 0;
+  private static async isEmailUnique(email: string): Promise<boolean> {
+    return (await User.find({ email }).exec()).length <= 0;
   }
 
-  private async save(doc: IUser) {
+  private static async save(doc: IUser) {
     return doc.save();
   }
 
-  private createToken(user: IUser, expiresIn = config.expiresIn) {
+  private static createToken(user: IUser, expiresIn = config.expiresIn) {
     let token = jwt.sign(
       {
         id: user._id,
