faebc4433f6d600f0cc89c53b2a1a96edc7fe084
Antonio <antonio.c.m.eu@gmail.com>
2023-10-28 01:29:44 +0100

    atualização de ficheiros

--- src/controllers/productController.ts
+++ src/controllers/productController.ts
@@ -3,12 +3,41 @@ import { Product } from "../models/productModel";
 import { Subcategory } from "../models/subcategoryModel";
 
 export class ProductController {
-    
+  
+  private validateRequest(req: Request): { isValid: boolean; message?: string } {
+    const { name, description, imageUrl, manufacturer, stockQuantity, price, subcategoryId } = req.body;
+
+    if (!name || !description || !imageUrl || !manufacturer || stockQuantity == null || price == null) {
+      return { isValid: false, message: "All fields are required" };
+    }
+
+    if (typeof price !== 'number' || price <= 0) {
+      return { isValid: false, message: "Price must be a positive number" };
+    }
+
+    if (!Number.isInteger(stockQuantity) || stockQuantity < 0) {
+      return { isValid: false, message: "Stock quantity must be a non-negative integer" };
+    }
+
+    if (!subcategoryId || !/^[0-9a-fA-F]{24}$/.test(subcategoryId)) {
+      return { isValid: false, message: "Invalid subcategory ID" };
+    }
+
+    try {
+      new URL(imageUrl);
+    } catch (_) {
+      return { isValid: false, message: "Invalid image URL" };
+    }
+
+    return { isValid: true };
+  }
+
   public async createProduct(req: Request, res: Response): Promise<Response> {
     const { name, description, imageUrl, manufacturer, stockQuantity, price, subcategoryId } = req.body;
 
-    if (!subcategoryId) {
-      return res.status(400).send({ message: "Subcategory ID is required" });
+    const { isValid, message } = this.validateRequest(req);
+    if (!isValid) {
+      return res.status(400).send({ message });
     }
 
     try {
@@ -34,8 +63,8 @@ export class ProductController {
 
       return res.status(201).send(savedProduct);
     } catch (error) {
-        console.log(error)
-      return res.status(500).send({ message: "Erro" });
+      console.log(error);
+      return res.status(500).send({ message: "Internal Server Error" });
     }
   }
 }
