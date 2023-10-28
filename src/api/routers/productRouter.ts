32b1538197d594a8218f13a563a91fde5b20e199
Antonio <antonio.c.m.eu@gmail.com>
2023-10-28 23:05:33 +0100

    alterações min

--- src/api/routers/productRouter.ts
+++ src/api/routers/productRouter.ts
@@ -1,14 +1,22 @@
-import express, { Router } from "express";
+import express from "express";
 import { ProductController } from "../../controllers/productController";
 
 const router = express.Router();
 const productController = new ProductController();
 
 // Rota para devolver todos os Produtos
-router.post('/', productController.createProduct.bind(productController));
+router.post('/', productController.createProduct);
+
+//Rota para dar update a um produto pelo ID
 router.put('/:id', productController.updateProduct);
+
+// Rota para eliminar um produto pelo ID
 router.delete('/:id', productController.deleteProduct);
+
+// Rota para devolver um produto pelo ID
 router.get('/:id', productController.getProductById);
+
+// Rota para devolver todos os produtos existentes
 router.get('/', productController.getAllProducts);
 
 export default router;
