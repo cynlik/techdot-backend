a5727c133a811427cdf61a85e03476fa975fd991
samusafe <samucarvalhocp@gmail.com>
2023-10-29 21:56:05 +0000

    user fix

--- src/api/routers/userRouter.ts
+++ src/api/routers/userRouter.ts
@@ -16,44 +16,8 @@ router.put('/verify', userController.verifyAccount);
 router.post("/login", userController.loginUser);
 
 router.route('/id/:userId')
-    .get(asyncHandler(async (req, res, next) => {
-        console.log("Get a user with user Id");
-        let userId = req.params.userId;
-
-        try {
-            let user = await userController.findById(userId);
-            res.status(200).send(user);
-        }catch(err: any) {
-            err = (err instanceof Error ? err : new Error(err));
-
-            res.status(404).send(`Error: \n ${err.message}`);
-        }
-    }))
-    .post(asyncHandler(async (req, res, next) => {
-        console.log("Update a user with user Id");
-        let userId = req.params.userId;
-        let newUser: Partial<IUser> = req.body;
-
-        try {
-            const user = await userController.updateById(userId, newUser);
-            res.status(200).send(user);
-        } catch (err: any) {
-            err = (err instanceof Error ? err : new Error(err));
-
-            res.status(404).send(`Error: \n ${err.message}`);
-        }
-    }))
-    .delete(asyncHandler(async (req, res, next) => {
-        console.log("Remove a user with user Id");
-        let userId = req.params.userId;
-
-        try {
-            const user = await userController.removeById(userId);
-            res.status(200).send(user);
-        } catch (err: any) {
-            err = (err instanceof Error ? err : new Error(err));
-
-            res.status(404).send(`Error: \n ${err.message}`);
-        }
-    }));
+    .get(userController.getUserById)
+    .post(userController.updateUserById)
+    .delete(userController.deleteUserById);
+    
 export default router;
