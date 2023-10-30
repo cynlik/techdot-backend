b8c55e32f94ad125014f9fd52e567bca3d63cee1
Antonio <antonio.c.m.eu@gmail.com>
2023-10-30 12:12:46 +0000

    alterações product | category | subcategory

--- src/api/routers/productRouter.ts
+++ src/api/routers/productRouter.ts
@@ -20,4 +20,16 @@ router.get('/:id', productController.getProductById);
 // Rota para devolver todos os produtos existentes
 router.get('/', productController.getAllProducts);
 
+//Rota para dar update a um produto pelo ID
+router.put('/:id', productController.updateProduct);
+
+// Rota para eliminar um produto pelo ID
+router.delete('/:id', productController.deleteProduct);
+
+// Rota para devolver um produto pelo ID
+router.get('/:id', productController.getProductById);
+
+// Rota para devolver todos os produtos existentes
+router.get('/', productController.getAllProducts);
+
 export default router;
