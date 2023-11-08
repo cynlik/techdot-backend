0b8c9d0077bf979b8180d9ed31a49996db1dcf91
Antonio <antonio.c.m.eu@gmail.com>
2023-11-08 15:05:27 +0000

    pro

--- src/controllers/productController.ts
+++ src/controllers/productController.ts
@@ -196,10 +196,10 @@ export class ProductController {
 
   public getProductsByName = async (req: Request, res: Response) => {
     const { name, sort, page = 1, limit = 6 } = req.query as {
-        name?: string;
-        sort: string;
-        page: string;
-        limit: string;
+      name?: string;
+      sort: string;
+      page: string;
+      limit: string;
     };
 
     const isAdmin = req.user && hasPermission(req.user.role, UserRole.Manager);
@@ -207,12 +207,12 @@ export class ProductController {
     const conditions: any = {};
 
     if (!isAdmin) {
-        conditions.visible = true;
+      conditions.visible = true;
     }
 
     if (name) {
-        const regex = new RegExp(name, 'i');
-        conditions.name = regex;
+      const regex = new RegExp(name, 'i');
+      conditions.name = regex;
     }
 
     const count = await Product.countDocuments(conditions);
@@ -221,54 +221,54 @@ export class ProductController {
     let query = Product.find(conditions);
 
     if (sort) {
-        switch (sort) {
-            case 'preco_maior':
-                query = query.sort({ price: -1 });
-                break;
-            case 'preco_menor':
-                query = query.sort({ price: 1 });
-                break;
-            default:
-                break;
-        }
+      switch (sort) {
+        case 'preco_maior':
+          query = query.sort({ price: -1 });
+          break;
+        case 'preco_menor':
+          query = query.sort({ price: 1 });
+          break;
+        default:
+          break;
+      }
     }
 
     let products = await query
-        .limit(Number(limit))
-        .skip((Number(page) - 1) * Number(limit));
+      .limit(Number(limit))
+      .skip((Number(page) - 1) * Number(limit));
 
     if (products.length === 0) {
-        res.status(404).send({ message: 'Nenhum produto encontrado.' });
+      res.status(404).send({ message: 'Nenhum produto encontrado.' });
     } else {
-        res.status(200).send({ products, totalPages });
+      res.status(200).send({ products, totalPages });
     }
   };
 
   public getProductById = async (req: Request, res: Response) => {
     const { id } = req.params;
     const validations = [() => this.validateId(id, Constant.Product)];
-  
+
     for (const validate of validations) {
       const errors = validate();
       const response = this.sendErrorResponse(errors, res);
       if (response) return response;
     }
-  
+
     try {
       const isAdmin = req.user && hasPermission(req.user.role, UserRole.Manager);
-      
+
       const conditions: any = { _id: id };
-      
+
       if (!isAdmin) {
         conditions.visible = true;
       }
-      
+
       const product = await Product.findOne(conditions);
-      
+
       if (!product) {
         return res.status(404).send({ message: 'Product not found' });
       }
-  
+
       return res.status(200).send(product);
     } catch (error) {
       console.error(error);
@@ -276,5 +276,4 @@ export class ProductController {
     }
   };
 
-  
 }
