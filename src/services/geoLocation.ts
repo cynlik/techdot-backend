b9331c594510ac34d6ba2cb4519707ea0d80bd3b
samusafe <samucarvalhocp@gmail.com>
2023-11-09 18:36:46 +0000

    geoLocation service implementation

---
+++ src/services/geoLocation.ts
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+export const getUserCountry = async (ip: string) => {
+    try {
+        const response = await axios.get(
+            `https://api.ipgeolocation.io/ipgeo?apiKey=${process.env.IPGEOAPI}&ip=${ip}`
+        );
+        
+        return response.data.country_name;
+    } catch (error) {
+        return null;
+    }
+};
