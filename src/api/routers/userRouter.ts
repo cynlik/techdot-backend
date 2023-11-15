89c3be0dfc1b1a64814a98ec40909b83bdb0ed0e
samusafe <samucarvalhocp@gmail.com>
2023-11-15 19:24:34 +0000

    validator implementation in userRoutes and userController

--- src/api/routers/userRouter.ts
+++ src/api/routers/userRouter.ts
@@ -2,22 +2,25 @@ import express from "express";
 import UserController from '@src/controllers/userController';
 import { roleMiddleware } from '@src/middlewares/roleMiddleware';
 import { UserRole } from "@src/utils/roles";
-import validateToken from '@src/middlewares/validateToken'; 
+import validateToken from '@src/middlewares/validateToken';
+import Validator from "@src/middlewares/validator";
+import { Constant } from "@src/utils/constant";
+import { User } from "@src/models/userModel";
 
 const router = express.Router();
 const userController = new UserController();
 
 // Register user
-router.post('/register', userController.registerUser);
+router.post('/register', Validator.validateBody(["name","email","password"]), userController.registerUser);
 
 // Verify user
 router.put('/verify', userController.verifyAccount);
 
 // Login
-router.post("/login", userController.loginUser);
+router.post("/login", Validator.validateBody(["email","password"]), userController.loginUser);
 
 // Forget
-router.post("/forgetpassword/:email", userController.forgetPassword);
+router.post("/forgetpassword", Validator.validateBody(["email"]), userController.forgetPassword);
 
 // Reset
 router.put("/resetpassword/:token", userController.resetPassword);
@@ -26,7 +29,7 @@ router.put("/resetpassword/:token", userController.resetPassword);
 router.get('/me', validateToken, userController.me);
 
 // User routes
-router.get('/:id?', validateToken, roleMiddleware(UserRole.Manager), userController.getUserById);
+router.get('/:id?', validateToken, roleMiddleware(UserRole.Manager), Validator.validateIds([{ paramName: "id", model: User, type: Constant.User, isOptional: true }]), userController.getUserById);
 router.put('/:id', validateToken, roleMiddleware(UserRole.Manager), userController.updateUserById);
 router.delete('/:id', validateToken, roleMiddleware(UserRole.Manager), userController.deleteUserById);
 
