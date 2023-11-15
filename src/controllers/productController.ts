0c5bc815cd411606dafc64f734456da379b590af
Antonio <antonio.c.m.eu@gmail.com>
2023-11-15 19:02:04 +0000

    validator | product

--- src/controllers/productController.ts
+++ src/controllers/productController.ts
@@ -47,21 +47,21 @@ export class ProductController {
   public updateProduct = async (req: Request, res: Response) => {
     const { id } = req.params;
     const updateFields = req.body;
-  
+
     try {
       const updatedProduct = await Product.findByIdAndUpdate(id, updateFields, { new: true, runValidators: true });
-  
+
       if (!updatedProduct) {
         return res.status(404).send({ message: "Product not found" });
       }
-  
+
       return res.status(200).send(updatedProduct);
     } catch (error) {
       console.error(error);
       return res.status(500).send({ message: "Internal Server Error" });
     }
   };
-  
+
   public deleteProduct = async (req: Request, res: Response) => {
     const { id } = req.params;
 
@@ -84,32 +84,32 @@ export class ProductController {
         page?: string;
         limit?: string;
       };
-      
+
       const pageNumber = parseInt(page, 10);
       const limitNumber = parseInt(limit, 10);
 
       if (isNaN(pageNumber) || pageNumber <= 0 || isNaN(limitNumber) || limitNumber <= 0) {
         return res.status(400).send({ message: 'Parâmetros de paginação inválidos.' });
       }
-  
+
       const isAdmin = req.user && hasPermission(req.user.role, UserRole.Manager);
-  
+
       const conditions: any = {};
-  
+
       if (!isAdmin) {
         conditions.visible = true;
       }
-  
+
       if (name) {
         const regex = new RegExp(name, 'i');
         conditions.name = regex;
       }
-  
+
       const count = await Product.countDocuments(conditions);
       const totalPages = Math.ceil(count / limitNumber);
-  
+
       let query = Product.find(conditions).populate({ path: 'subcategoryId', populate: [{ path: 'category' }] });
-  
+
       switch (sort) {
         case 'preco_maior':
           query = query.sort({ price: -1 });
@@ -118,11 +118,11 @@ export class ProductController {
           query = query.sort({ price: 1 });
           break;
       }
-  
+
       const products = await query
         .limit(limitNumber)
         .skip((pageNumber - 1) * limitNumber);
-  
+
       if (products.length === 0) {
         res.status(404).send({ message: 'Nenhum produto encontrado.' });
       } else {
