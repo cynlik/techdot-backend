2660a9fb303b56a1a6a282aca8cd21b2328082a8
Antonio <antonio.c.m.eu@gmail.com>
2023-11-18 18:21:00 +0000

    category

--- src/api/routers/categoryRouter.ts
+++ src/api/routers/categoryRouter.ts
@@ -1,13 +1,30 @@
 import express from "express";
 import { CategoryController } from "@src/controllers/categoryController";
+import Validator from "@src/middlewares/validator";
+import validateToken from "@src/middlewares/validateToken";
+import { roleMiddleware } from "@src/middlewares/roleMiddleware";
+import { UserRole } from "@src/utils/roles";
+import { Constant } from "@src/utils/constant";
+import { Category } from "@src/models/categoryModel";
 
 const router = express.Router();
 const categoryController = new CategoryController();
 
-// Rota para cirar uma Categoria 
-router.post('/', categoryController.createCategory);
+// Rotas Publicas
 
 // Rota para obter todas as categorias
 router.get('/', categoryController.getAllCategory)
+
+// Rotas Admin
+
+// Rota para cirar uma Categoria 
+router.post('/', validateToken, roleMiddleware(UserRole.Manager), Validator.validateFields({ required: ['name']}), categoryController.createCategory);
+
+// Rota para dar update no nome da categoria
+router.put('/update/:id', validateToken, roleMiddleware(UserRole.Manager), Validator.validateFields({ optional: ['name']}), Validator.validateIds([{ paramName: "id", model: Category, type: Constant.Category }]), categoryController.updateCategory)
+
+// Rota para eliminar uma categoria pelo seu id
+router.delete('/delete/:id', validateToken, roleMiddleware(UserRole.Manager), Validator.validateIds([{ paramName: "id", model: Category, type: Constant.Category}]))
+
 
 export default router;
--- src/controllers/categoryController.ts
+++ src/controllers/categoryController.ts
@@ -19,11 +19,43 @@ export class CategoryController {
 
   public async getAllCategory(req: Request, res:Response) {
     try {
-      const categories = await Category.find().populate({ path: 'subcategory', populate: [{ path: 'products'}] });
+      const categories = await Category.find();
       return res.status(200).send(categories)
     } catch (error) {
       return res.status(500).send({ message: 'Internal Error' })
     }
   }
-  // Adicionar updateCategory | getCategoryByID 
+
+  public updateCategory = async(req: Request, res: Response)  => {
+    const { id } = req.params;
+    const updateFields = req.body;
+
+    try {
+      const updateCategory = await Category.findByIdAndUpdate(id, updateFields, { new: true , runValidators: true});
+
+      if (!updateCategory) {
+        res.status(404).send({message: 'Category not found!'})
+      }
+
+      return res.status(200).send(updateCategory);
+    } catch (error) {
+      return res.status(500).send({ message: 'Internal Error'})
+    }
+  }
+
+  public deleteCategory = async (req: Request, res: Response) => {
+    const { id } = req.params
+
+    try {
+      await Category.deleteOne({ _id: id});
+
+      // ao eliminar uma categoria vai ter que eliminar na Categories e na Subcategories
+      // algterar e adicionar visible para ambos, e ao eliminar todas as subcategorias de dentro da categoria teria que ficar visible: false || não permitir eliminar enquanto tem a subcategoria associada a categoria (cliente)
+      
+      return res.status(200).send({ message: 'Category deleted successfully'})
+    } catch (error) {
+      return res.status(500).send({ message: 'Internal Error'})
+    }
+  }
+
 }
