28e60f5aeabb6d143459bfdaaac645c924224d9a
samusafe <samucarvalhocp@gmail.com>
2023-12-03 19:00:58 +0000

    userCart + userWishList

--- src/api/routers/userRouter.ts
+++ src/api/routers/userRouter.ts
@@ -5,6 +5,7 @@ import validateToken from '@src/middlewares/validateToken';
 import Validator from "@src/middlewares/validator";
 import { Constant } from "@src/utils/constant";
 import { User, UserStatus } from "@src/models/userModel";
+import { Product } from "@src/models/productModel";
 
 const router = express.Router();
 const userController = new UserController();
@@ -31,6 +32,21 @@ router.get('/me', validateToken(), userController.me);
 
 // Update my information
 router.put('/me', validateToken(), Validator.validateFields({ optional: ["name","password","picture","address","country"]}), userController.meUpdate)
+
+// Add item to wish list
+router.post("/wishlist/:id", validateToken(), Validator.validateIds([{ paramName: "id", model: Product, type: Constant.Product }]), userController.addToWishList);
+
+// Get wish list
+router.get("/wishlist", validateToken(), userController.getWishList);
+
+// Add item to cart
+router.post("/cart/:id", validateToken(), Validator.validateIds([{ paramName: "id", model: Product, type: Constant.Product }]), userController.addToCart);
+
+// Get cart items
+router.get("/cart", validateToken(), userController.getCartItems);
+
+// Update cart items
+router.put("/cart", validateToken(), Validator.validateFields({ required: ["id", "action"], optional: ["quantity"] }), userController.updateCart);
 
 // Logout
 router.post('/logout', validateToken(), userController.logout);
