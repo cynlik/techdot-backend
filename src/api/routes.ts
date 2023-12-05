38f162ffcf56f88a6d6102cac01c799913d79011
Antonio <antonio.c.m.eu@gmail.com>
2023-12-05 02:03:43 +0000

    router

--- src/api/routes.ts
+++ src/api/routes.ts
@@ -3,11 +3,13 @@ import productRouter from "./routers/productRouter";
 import categoryRouter from "./routers/categoryRouter";
 import subcategoryRouter from "./routers/subcategoryRouter";
 import cartRouter from "./routers/cartRouter";
+import discountRouter from "./routers/discountRouter"
 
 export const routes = [
   { path: "/api/user", router: userRouter },
   { path: "/api/product", router: productRouter },
   { path: "/api/category", router: categoryRouter },
   { path: "/api/subcategory", router: subcategoryRouter },
+  { path: "/api/discount", router: discountRouter },
   { path: "/api/cart", router: cartRouter }
 ];
