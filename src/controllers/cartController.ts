6fd6a5122061fe0c80939e3f01803887a57dda1e
samusafe <samucarvalhocp@gmail.com>
2023-12-07 14:05:38 +0000

    cookie and security

--- src/controllers/cartController.ts
+++ src/controllers/cartController.ts
@@ -2,7 +2,11 @@ import * as _ from "lodash";
 import { Request, Response } from "express";
 import { User, IUser } from "@src/models/userModel";
 import { CustomError } from "@src/utils/customError";
-import { HttpStatus, ERROR_MESSAGES, SUCCESS_MESSAGES } from "@src/utils/constant";
+import {
+	HttpStatus,
+	ERROR_MESSAGES,
+	SUCCESS_MESSAGES,
+} from "@src/utils/constant";
 import { Product } from "@src/models/productModel";
 import { CartItem, ShoppingCart } from "@src/models/cartModel";
 
@@ -22,26 +26,90 @@ export default class CartController {
 			const product = await Product.findById(req.params.id);
 
 			if (quantity <= 0) {
-				return next(new CustomError(HttpStatus.BAD_REQUEST, ERROR_MESSAGES.INVALID_QUANTITY));
+				return next(
+					new CustomError(
+						HttpStatus.BAD_REQUEST,
+						ERROR_MESSAGES.INVALID_QUANTITY
+					)
+				);
 			}
 
 			if (!product) {
-				return next(new CustomError(HttpStatus.NOT_FOUND, ERROR_MESSAGES.PRODUCT_NOT_FOUND));
+				return next(
+					new CustomError(
+						HttpStatus.NOT_FOUND,
+						ERROR_MESSAGES.PRODUCT_NOT_FOUND
+					)
+				);
 			}
 
 			if (product.stockQuantity === 0) {
-				return next(new CustomError(HttpStatus.BAD_REQUEST, ERROR_MESSAGES.OUT_OF_STOCK));
+				return next(
+					new CustomError(HttpStatus.BAD_REQUEST, ERROR_MESSAGES.OUT_OF_STOCK)
+				);
+			}
+
+			if (!req.session) {
+				return next(
+					new CustomError(
+						HttpStatus.BAD_REQUEST,
+						ERROR_MESSAGES.UNEXPECTED_ERROR
+					)
+				);
 			}
 
 			if (!req.user) {
-				// Lógica para usuário não autenticado
+				if (!req.session.cart) {
+					req.session.cart = { items: [], total: 0 };
+				}
+
+				const guest = req.session.cart;
+
+				const existingCartItemIndex = guest.items.findIndex(
+					(item: { product: any }) => item.product === product.id
+				);
+
+				if (existingCartItemIndex !== -1) {
+					guest.items[existingCartItemIndex].quantity += parsedQuantity || 1;
+					guest.items[existingCartItemIndex].totalPrice = (
+						product.price * guest.items[existingCartItemIndex].quantity
+					).toFixed(2);
+				} else {
+					const newItem: CartItem = {
+						product: product.id,
+						quantity: parsedQuantity || 1,
+						totalPrice: (product.price * parsedQuantity).toFixed(2),
+					} as CartItem;
+
+					guest.items.push(newItem);
+				}
+
+				const newCartTotal = guest.items
+					.reduce(
+						(total: number, cartItem: { totalPrice: string }) =>
+							total + parseFloat(cartItem.totalPrice),
+						0
+					)
+					.toFixed(2);
+
+				guest.total = parseFloat(newCartTotal);
+
+				res.status(HttpStatus.OK).json({
+					message: ERROR_MESSAGES.INTERNAL_SERVER_ERROR,
+					cart: guest.items || [],
+					cartTotal: guest.total || 0,
+				});
+
+				req.session.cart = guest;
 			} else {
 				const user = await User.findById(req.user.id).populate(
 					"cart.items.product"
 				);
 
 				if (!user) {
-					return next(new CustomError(HttpStatus.NOT_FOUND, ERROR_MESSAGES.USER_NOT_FOUND));
+					return next(
+						new CustomError(HttpStatus.NOT_FOUND, ERROR_MESSAGES.USER_NOT_FOUND)
+					);
 				}
 
 				const userCart: ShoppingCart | null = user.cart || {
@@ -84,7 +152,9 @@ export default class CartController {
 				const updatedUser = await User.findById(user.id).populate("cart.items");
 
 				if (!updatedUser) {
-					return next(new CustomError(HttpStatus.NOT_FOUND, ERROR_MESSAGES.USER_NOT_FOUND));
+					return next(
+						new CustomError(HttpStatus.NOT_FOUND, ERROR_MESSAGES.USER_NOT_FOUND)
+					);
 				}
 
 				res.status(HttpStatus.OK).json({
@@ -95,7 +165,12 @@ export default class CartController {
 			}
 		} catch (error) {
 			console.error(error);
-			return next(new CustomError(HttpStatus.INTERNAL_SERVER_ERROR, ERROR_MESSAGES.INTERNAL_SERVER_ERROR));
+			return next(
+				new CustomError(
+					HttpStatus.INTERNAL_SERVER_ERROR,
+					ERROR_MESSAGES.INTERNAL_SERVER_ERROR
+				)
+			);
 		}
 	};
 
@@ -105,27 +180,53 @@ export default class CartController {
 		next: Function
 	) => {
 		try {
-			const user = await User.findById(req.user.id).populate("cart.items");
+			if (!req.user) {
+				const savedCart = req.session;
 
-			if (!user) {
-				return next(new CustomError(HttpStatus.NOT_FOUND, ERROR_MESSAGES.USER_NOT_FOUND));
+				if (!savedCart || !savedCart.cart || !savedCart.cart.items) {
+					return res
+						.status(HttpStatus.OK)
+						.json({ message: SUCCESS_MESSAGES.EMPTY_CART });
+				}
+
+				return res.status(HttpStatus.OK).json({
+					cart: savedCart.cart.items.map(
+						(cartItem: { product: any; quantity: any; totalPrice: any }) => ({
+							product: cartItem.product,
+							quantity: cartItem.quantity,
+							total: cartItem.totalPrice,
+						})
+					),
+					cartTotal: savedCart.cart.total,
+				});
 			}
 
-			if (!user.cart) {
-				res.status(HttpStatus.OK).json({ message: SUCCESS_MESSAGES.EMPTY_CART });
-			} else {
-				res.status(HttpStatus.OK).json({
-					cart: user.cart.items.map((cartItem) => ({
+			const user = await User.findById(req.user.id).populate("cart.items");
+
+			if (!user || !user.cart || !user.cart.items) {
+				return res
+					.status(HttpStatus.OK)
+					.json({ message: SUCCESS_MESSAGES.EMPTY_CART });
+			}
+
+			return res.status(HttpStatus.OK).json({
+				cart: user.cart.items.map(
+					(cartItem: { product: any; quantity: any; totalPrice: any }) => ({
 						product: cartItem.product,
 						quantity: cartItem.quantity,
 						total: cartItem.totalPrice,
-					})),
-					cartTotal: user.cart.total,
-				});
-			}
+					})
+				),
+				cartTotal: user.cart.total,
+			});
 		} catch (error) {
 			console.log(error);
-			return next(new CustomError(HttpStatus.INTERNAL_SERVER_ERROR, ERROR_MESSAGES.INTERNAL_SERVER_ERROR));
+			return next(
+				new CustomError(
+					HttpStatus.INTERNAL_SERVER_ERROR,
+					ERROR_MESSAGES.INTERNAL_SERVER_ERROR
+				)
+			);
 		}
 	};
 
@@ -135,136 +236,222 @@ export default class CartController {
 		next: Function
 	) => {
 		try {
-			const user = await User.findById(req.user.id).populate("cart.items");
+			if (!req.session) {
+				return next(
+					new CustomError(
+						HttpStatus.BAD_REQUEST,
+						ERROR_MESSAGES.UNEXPECTED_ERROR
+					)
+				);
+			}
+
 			const { id, quantity, action } = req.body;
 			const parsedQuantity = parseInt(quantity, 10);
 
-			if (!user) {
-				return next(new CustomError(HttpStatus.NOT_FOUND, ERROR_MESSAGES.USER_NOT_FOUND));
+			if (action === "removeAll" && (id || quantity)) {
+				return next(
+					new CustomError(
+						HttpStatus.BAD_REQUEST,
+						ERROR_MESSAGES.REMOVE_PRODUCT_ID +
+							ERROR_MESSAGES.REMOVE_PRODUCT_QUANTITY
+					)
+				);
 			}
 
-			const userCart: ShoppingCart | null = user.cart || {
-				items: [],
-				total: 0,
-			};
-
-			if (action === "removeAll") {
-				if (id) {
-					return next(
-						new CustomError(
-							HttpStatus.BAD_REQUEST,
-							ERROR_MESSAGES.REMOVE_PRODUCT_ID
-						)
-					);
-				} else if (quantity) {
-					return next(
-						new CustomError(
-							HttpStatus.BAD_REQUEST,
-							ERROR_MESSAGES.REMOVE_PRODUCT_QUANTITY
-						)
-					);
-				}
-				userCart.items = [];
-			} else {
-				if (!quantity) {
-					return next(new CustomError(HttpStatus.BAD_REQUEST, ERROR_MESSAGES.QUANTITY_REQUIRED));
-				}
-
-				if (!id) {
-					return next(new CustomError(HttpStatus.BAD_REQUEST, ERROR_MESSAGES.PRODUCT_ID_REQUIRED));
-				}
-
-				const product = await Product.findById(id);
-
-				if (!product) {
-					return next(
-						new CustomError(HttpStatus.NOT_FOUND, ERROR_MESSAGES.PRODUCT_NOT_FOUND)
-					);
-				}
-
-				if (product.stockQuantity === 0) {
-					return next(new CustomError(HttpStatus.BAD_REQUEST, ERROR_MESSAGES.OUT_OF_STOCK));
-				}
-
-				const existingCartItem = userCart.items.find((item) =>
-					item.product.equals(product.id)
+			if (action !== "removeAll" && (!id || !quantity || !action)) {
+				return next(
+					new CustomError(
+						HttpStatus.BAD_REQUEST,
+						ERROR_MESSAGES.REMOVE_PRODUCT_ID + ERROR_MESSAGES.REMOVE_PRODUCT_ID
+					)
 				);
+			}
 
-				if (!existingCartItem) {
-					return next(
-						new CustomError(HttpStatus.NOT_FOUND, ERROR_MESSAGES.PRODUCT_NOT_IN_CART)
+			if (!req.user) {
+				const guestCart = req.session.cart || { items: [], total: 0 };
+
+				if (action === "removeAll") {
+					guestCart.items = [];
+				} else {
+					const existingCartItemIndex = guestCart.items.findIndex(
+						(item: { product: any }) => item.product === id
 					);
-				}
 
-				if (action === "add") {
-					if (existingCartItem) {
-						existingCartItem.quantity += parsedQuantity || 1;
-						existingCartItem.totalPrice = (
-							product.price * existingCartItem.quantity
-						).toFixed(2);
-					} else {
-						const newItem: CartItem = {
-							product: product.id,
-							quantity: parsedQuantity || 1,
-							totalPrice: (product.price * parsedQuantity).toFixed(2),
-						} as CartItem;
+					const product = await Product.findById(id);
 
-						userCart.items.push(newItem);
-					}
-				} else if (action === "remove") {
-					if (existingCartItem) {
-						if (existingCartItem.quantity >= quantity) {
-							existingCartItem.quantity -= quantity;
-							existingCartItem.totalPrice = (
-								product.price * existingCartItem.quantity
-							).toFixed(2);
-						} else {
-							userCart.items = userCart.items.filter(
-								(item) => !item.product.equals(product.id)
-							);
-						}
-					}
-				} else if (action === "removeProduct") {
-					if (quantity) {
+					if (!product) {
 						return next(
 							new CustomError(
-								HttpStatus.BAD_REQUEST,
-								ERROR_MESSAGES.REMOVE_PRODUCT_QUANTITY
+								HttpStatus.NOT_FOUND,
+								ERROR_MESSAGES.PRODUCT_NOT_FOUND
 							)
 						);
 					}
-					userCart.items = userCart.items.filter(
-						(item) => !item.product.equals(product.id)
+
+					if (existingCartItemIndex !== -1) {
+						const existingCartItem = guestCart.items[existingCartItemIndex];
+
+						if (action === "add") {
+							existingCartItem.quantity += parsedQuantity || 1;
+							existingCartItem.totalPrice = (
+								product.price * existingCartItem.quantity
+							).toFixed(2);
+						} else if (action === "remove") {
+							if (existingCartItem.quantity >= quantity) {
+								existingCartItem.quantity -= quantity;
+								existingCartItem.totalPrice = (
+									product.price * existingCartItem.quantity
+								).toFixed(2);
+							} else {
+								guestCart.items.splice(existingCartItemIndex, 1);
+							}
+						} else if (action === "removeProduct") {
+							guestCart.items.splice(existingCartItemIndex, 1);
+						}
+					} else {
+						if (action === "add") {
+							const newItem: CartItem = {
+								product: id,
+								quantity: parsedQuantity || 1,
+								totalPrice: (product.price * parsedQuantity).toFixed(2),
+							} as CartItem;
+
+							guestCart.items.push(newItem);
+						} else {
+							return next(
+								new CustomError(
+									HttpStatus.NOT_FOUND,
+									ERROR_MESSAGES.PRODUCT_NOT_IN_CART
+								)
+							);
+						}
+					}
+				}
+
+				const newCartTotal = guestCart.items
+					.reduce(
+						(total: number, cartItem: { totalPrice: string }) =>
+							total + parseFloat(cartItem.totalPrice),
+						0
+					)
+					.toFixed(2);
+
+				guestCart.total = parseFloat(newCartTotal);
+
+				req.session.cart = guestCart;
+
+				return res.status(HttpStatus.OK).json({
+					message: SUCCESS_MESSAGES.UPDATED_SUCCESSFULLY,
+					cart: guestCart.items,
+					cartTotal: guestCart.total,
+				});
+			} else {
+				const user = await User.findById(req.user.id).populate("cart.items");
+
+				if (!user) {
+					return next(
+						new CustomError(HttpStatus.NOT_FOUND, ERROR_MESSAGES.USER_NOT_FOUND)
 					);
 				}
-			}
 
-			const newCartTotal = userCart.items
-				.reduce((total, cartItem) => total + parseFloat(cartItem.totalPrice), 0)
-				.toFixed(2);
+				const userCart: ShoppingCart | null = user.cart || {
+					items: [],
+					total: 0,
+				};
 
-			userCart.total = parseFloat(newCartTotal);
+				if (action === "removeAll") {
+					userCart.items = [];
+				} else {
+					const existingCartItemIndex = userCart.items.findIndex((item) =>
+						item.product.equals(id)
+					);
 
-			await User.findByIdAndUpdate(user.id, {
-				$set: { cart: { items: userCart.items, total: userCart.total } },
-			});
+					const product = await Product.findById(id);
 
-			const updatedUser = await User.findById(user.id).populate("cart.items");
+					if (!product) {
+						return next(
+							new CustomError(
+								HttpStatus.NOT_FOUND,
+								ERROR_MESSAGES.PRODUCT_NOT_FOUND
+							)
+						);
+					}
 
-			if (!updatedUser) {
-				return res.status(HttpStatus.NOT_FOUND).json({
-					message: ERROR_MESSAGES.USER_NOT_FOUND,
+					if (existingCartItemIndex !== -1) {
+						const existingCartItem = userCart.items[existingCartItemIndex];
+
+						if (action === "add") {
+							existingCartItem.quantity += parsedQuantity || 1;
+							existingCartItem.totalPrice = (
+								product.price * existingCartItem.quantity
+							).toFixed(2);
+						} else if (action === "remove") {
+							if (existingCartItem.quantity >= quantity) {
+								existingCartItem.quantity -= quantity;
+								existingCartItem.totalPrice = (
+									product.price * existingCartItem.quantity
+								).toFixed(2);
+							} else {
+								userCart.items.splice(existingCartItemIndex, 1);
+							}
+						} else if (action === "removeProduct") {
+							userCart.items.splice(existingCartItemIndex, 1);
+						}
+					} else {
+						if (action === "add") {
+							const newItem: CartItem = {
+								product: id,
+								quantity: parsedQuantity || 1,
+								totalPrice: (product.price * parsedQuantity).toFixed(2),
+							} as CartItem;
+
+							userCart.items.push(newItem);
+						} else {
+							return next(
+								new CustomError(
+									HttpStatus.NOT_FOUND,
+									ERROR_MESSAGES.PRODUCT_NOT_IN_CART
+								)
+							);
+						}
+					}
+				}
+
+				const newCartTotal = userCart.items
+					.reduce(
+						(total, cartItem) => total + parseFloat(cartItem.totalPrice),
+						0
+					)
+					.toFixed(2);
+
+				userCart.total = parseFloat(newCartTotal);
+
+				await User.findByIdAndUpdate(user.id, {
+					$set: { cart: { items: userCart.items, total: userCart.total } },
+				});
+
+				const updatedUser = await User.findById(user.id).populate("cart.items");
+
+				if (!updatedUser) {
+					return res.status(HttpStatus.NOT_FOUND).json({
+						message: ERROR_MESSAGES.USER_NOT_FOUND,
+					});
+				}
+
+				return res.status(HttpStatus.OK).json({
+					message: SUCCESS_MESSAGES.UPDATED_SUCCESSFULLY,
+					cart: updatedUser.cart.items,
+					cartTotal: updatedUser.cart.total,
 				});
 			}
-
-			res.status(HttpStatus.OK).json({
-				message: SUCCESS_MESSAGES.UPDATED_SUCCESSFULLY,
-				cart: updatedUser.cart.items,
-				cartTotal: updatedUser.cart.total,
-			});
 		} catch (error) {
 			console.error(error);
-			return next(new CustomError(HttpStatus.INTERNAL_SERVER_ERROR, ERROR_MESSAGES.INTERNAL_SERVER_ERROR));
+			return next(
+				new CustomError(
+					HttpStatus.INTERNAL_SERVER_ERROR,
+					ERROR_MESSAGES.INTERNAL_SERVER_ERROR
+				)
+			);
 		}
 	};
 }
--- src/index.ts
+++ src/index.ts
@@ -5,21 +5,32 @@ import { routes } from "@src/api/routes";
 import "./config"
 import { errorHandler } from './middlewares/errorHandler';
 import swaggerDocs from '@src/utils/swagger';
+import cookieSession from 'cookie-session';
 
 dotenv.config();
 
 const app = express();
+const helmet = require('helmet')
 
 const hostname = String(process.env.HOST ?? "127.0.0.1");
 const port = Number(process.env.PORT ?? 3000);
 
 app.use(cors());
 app.use(express.json());
+app.use(helmet());
+app.use(cookieSession({
+  name: 'session',
+  keys: [
+    process.env.COOKIE_KEY1 as string,
+    process.env.COOKIE_KEY2 as string
+  ]
+}));
 
 for (const route of routes) {
   app.use(route.path, route.router);
 }
 app.use(errorHandler)// Tratamento de erros
+app.disable('x-powered-by');
 
 app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
