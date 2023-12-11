f2d9f9fb2938bf753a92d1bb6708eb5d4213cb7e
Antonio <antonio.c.m.eu@gmail.com>
2023-12-11 23:27:20 +0000

    update

--- src/models/productSpecifications/index.ts
+++ src/models/productSpecifications/index.ts
@@ -1,8 +1,8 @@
-import caseSpecificationSchema, { ICaseSpecifications } from "./case";
-import cpuSpecificationsSchema, { ICpuSpecifications } from "./cpu";
-import gpuSpecificationSchema, { IGpuSpecifications } from "./gpu";
-import motherboardSpecificationSchema, { IMotherboardSpecifications } from "./motherboard";
-import ramSpecificationSchema, { IRamSpecifications } from "./ram";
+import caseSpecificationSchema, { ICaseSpecifications } from "./models/case";
+import cpuSpecificationsSchema, { ICpuSpecifications } from "./models/cpu";
+import gpuSpecificationSchema, { IGpuSpecifications } from "./models/gpu";
+import motherboardSpecificationSchema, { IMotherboardSpecifications } from "./models/motherboard";
+import ramSpecificationSchema, { IRamSpecifications } from "./models/ram";
 
 export {
     caseSpecificationSchema,
