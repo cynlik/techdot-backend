19594cfcc283bf8d1583164edfb7070b80cc7f16
Guilherme <unitrabalho13@gmail.com>
2023-12-11 21:31:02 +0000

    .

--- src/models/saleModel.ts
+++ src/models/saleModel.ts
@@ -15,29 +15,25 @@ export enum SaleStatus {
 }
 
 export interface ISale extends Document {
-  userName: String;
-  userEmail: String;
-  userAdress: String;
-  userPhone: String;
-  paymentMethod: String;
-  shoppingCart: ShoppingCart;
-  date: Date;
-  // status: SaleStatus;
+        userName: String;
+        userEmail: String;
+        userAdress: String;
+        userPhone: String;
+        paymentMethod: String;
+        shoppingCart: typeof shoppingCartSchema;
+        date: Date;
+        status: SaleStatus;
 }
 
 export const saleSchema = new Schema<ISale>({
-  userName: { type: String, required: true },
-  userEmail: { type: String, required: true },
-  userAdress: { type: String, required: true },
-  userPhone: { type: String, required: true },
-  paymentMethod: { type: String, required: true },
-  shoppingCart: {
-    type: Schema.Types.ObjectId,
-    ref: 'ShoppingCart',
-    required: true,
-  },
-  date: { type: Date, default: Date.now() },
-  // status: { type: String, enum: Object.values(SaleStatus), default: SaleStatus.Pending },
+        userName: {type: String, required: true},
+        userEmail: {type: String, required: true},
+        userAdress: {type: String, required: true},
+        userPhone: {type: String, required: true},
+        paymentMethod: {type: String, required: true},
+        shoppingCart: { type: shoppingCartSchema, required: true },
+        date: {type: Date, default: Date.now()},
+        status: { type: String, enum: Object.values(SaleStatus), default: SaleStatus.Pending },
 });
 
 export const SaleModel = model<ISale>('Sales', saleSchema);
