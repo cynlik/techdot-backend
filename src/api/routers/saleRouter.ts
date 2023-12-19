5055e2c1b287640cd1ef8bc546989da7772fdcc3
Leandro Magalhães <57193254+cynlik@users.noreply.github.com>
2023-12-19 12:07:26 +0000

    swagger: cancel sale route

--- src/api/routers/saleRouter.ts
+++ src/api/routers/saleRouter.ts
@@ -1,7 +1,5 @@
 import { SaleController } from '@src/controllers/saleController';
 import express from 'express';
-import { roleMiddleware } from '@src/middlewares/roleMiddleware';
-import { UserStatus } from '@src/models/userModel';
 import validateToken from '@src/middlewares/validateToken';
 import Validator from '@src/middlewares/validator';
 import { Constant } from '@src/utils/constant';
@@ -44,6 +42,31 @@ router.post(
 // Get all sales and by email
 router.get('/all', validateToken(), saleController.getSalesByName);
 
+/**
+ * @openapi
+ * /api/sale/cancel/{id}:
+ *   put:
+ *     tags:
+ *       - Sale Routes
+ *     summary: Cancel a specific sale by ID
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *     responses:
+ *      200:
+ *        description: Sale cancelled successfully
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Sale'
+ *      400:
+ *        description: Invalid ID supplied
+ *      404:
+ *        description: Sale not found
+ */
 // Cancel Sale
 router.put('/cancel/:id', validateToken(), Validator.validateIds([{ paramName: 'id', model: SaleModel, type: Constant.Sale }]), saleController.cancel);
 
